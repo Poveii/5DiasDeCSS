e3eba019637869c53fe6447ee891431c0bf312cd
Poveii <yellowtutors@gmail.com>
2023-01-13 14:58:01 -0300

    :wrench::rocket: chore: Inserted Day01 route to deploy

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(root, "index.html"),
+        day01: resolve(root, "day01", "index.html"),
       },
     },
   },
