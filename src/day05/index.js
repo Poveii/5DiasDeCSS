00dbaa92bff4f3c1aad06707747bdeafe26c3cb6
Poveii <yellowtutors@gmail.com>
2023-02-27 19:22:36 -0300

    feat: Day 05 Structure

---
+++ src/day05/index.js
@@ -0,0 +1,17 @@
+import "./style.scss";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Star Rating</h1>
+
+  <main>
+    <p>Avalie esses desafios:</p>
+
+    <div class="stars">
+      <i class="ph-star"></i>
+      <i class="ph-star"></i>
+      <i class="ph-star"></i>
+      <i class="ph-star"></i>
+      <i class="ph-star"></i>
+    </div>
+  </main>
+`;
