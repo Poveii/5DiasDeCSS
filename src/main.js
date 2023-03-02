b5e940a1a952f7ab37aa1fb589c420e1fa3490ce
Poveii <yellowtutors@gmail.com>
2023-03-01 21:55:25 -0300

    fix: Changed import projects photos way

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import "./style.scss";
 
+import day01Photo from "./assets/day01-photo.png";
+import day02Photo from "./assets/day02-photo.png";
+import day03Photo from "./assets/day03-photo.png";
+import day04Photo from "./assets/day04-photo.png";
+import day05Photo from "./assets/day05-photo.png";
+
 document.querySelector("#app").innerHTML = `
   <header>
     <h1>5 dias de (S)CSS</h1>
@@ -12,31 +18,31 @@ document.querySelector("#app").innerHTML = `
   <main>
     <article>
       <a href="/day01/">
-      <img src="./assets/day01-photo.png" alt="" />
+      <img src="${day01Photo}" alt="" />
         <span>Dia 01</span>
       </a>
     </article>
     <article>
       <a href="/day02/">
-      <img src="./assets/day02-photo.png" alt="" />
+      <img src="${day02Photo}" alt="" />
         <span>Dia 02</span>
       </a>
     </article>
     <article>
       <a href="/day03/">
-      <img src="./assets/day03-photo.png" alt="" />
+      <img src="${day03Photo}" alt="" />
         <span>Dia 03</span>
       </a>
     </article>
     <article>
       <a href="/day04/">
-      <img src="./assets/day04-photo.png" alt="" />
+      <img src="${day04Photo}" alt="" />
         <span>Dia 04</span>
       </a>
     </article>
     <article>
       <a href="/day05/">
-      <img src="./assets/day05-photo.png" alt="" />
+      <img src="${day05Photo}" alt="" />
         <span>Dia 05</span>
       </a>
     </article>
